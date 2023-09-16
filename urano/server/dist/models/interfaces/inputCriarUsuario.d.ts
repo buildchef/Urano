5acda0164f895f218fbae58445a6451d1d180e9b
grievous0 <jvopereira2004@gmail.com>
2023-09-16 14:10:25 -0300

    add airplane routes + some adjusts

--- urano/server/dist/models/interfaces/inputCriarUsuario.d.ts
+++
@@ -1,9 +0,0 @@
-import { Cargos } from "../enums/cargos";
-export interface IInputCriarUsuario {
-    nome: string;
-    email: string;
-    telefone: string;
-    cargo?: Cargos | string;
-    cpf: string;
-    emailUsuarioLogado: string;
-}
